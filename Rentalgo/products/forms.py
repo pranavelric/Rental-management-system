3e061e22a334d7fbcfd6efac7f81e0beb13f4903
Shrey Goel <45508538+shreygoel7@users.noreply.github.com>
2019-07-10 22:49:28 +0530

    update forms.py

--- Rentalgo/products/forms.py
+++ Rentalgo/products/forms.py
@@ -1,21 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Product, Order, BuyOrder
-from django.forms.widgets import SelectDateWidget
 
-# class UserForm(forms.ModelForm):
-#     password = forms.CharField(widget=forms.PasswordInput())
-#
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'password')
-#
-# class UserProfileInfoForm(forms.ModelForm):
-#     class Meta:
-#         model = UserProfileInfo
-#         fields = ('portfolio_site', 'profile_pic')
-
-# add the form by seeing the above content and then delete the above content
 class ProdForm(forms.ModelForm):
     class Meta:
         model = Product
