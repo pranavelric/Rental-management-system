8e4b4fabe51fc2bd88c058bd1ecdea63a015fff2
Shrey Goel <45508538+shreygoel7@users.noreply.github.com>
2019-07-10 18:19:39 +0530

    adding files

--- Rentalgo/products/forms.py
+++ Rentalgo/products/forms.py
@@ -1,18 +1,56 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Product
+from .models import Product, Order, BuyOrder
+from django.forms.widgets import SelectDateWidget
 
+# class UserForm(forms.ModelForm):
+#     password = forms.CharField(widget=forms.PasswordInput())
+#
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password')
+#
+# class UserProfileInfoForm(forms.ModelForm):
+#     class Meta:
+#         model = UserProfileInfo
+#         fields = ('portfolio_site', 'profile_pic')
+
+# add the form by seeing the above content and then delete the above content
 class ProdForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('prod_name','description','img1','img2','dprice','wprice','mprice','available_for_selling','sprice')
+        fields = ('prod_name','description','img1','img2','quantity','dprice','wprice','mprice','available_for_selling','sprice')
         labels = {
                     'prod_name': 'Product Name',
                     'description': 'Description',
+                    'quantity': 'Quantity',
                     'img1': 'Image 1',
                     'img2': 'Image 2',
                     'dprice': 'Rent per Day',
                     'wprice': 'Rent per Week',
                     'mprice': 'Rent per Month',
                     'sprice': 'Selling Price(Fill if available for sale)',
+        }
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('address','city','zip_code','contact','date_range',)
+        labels = {
+                    'date_range': 'Date Range',
+                    'address': 'Address',
+                    'city': 'City',
+                    'zip_code': 'Zip Code',
+                    'contact': 'Mobile Number',
+        }
+
+class BuyForm(forms.ModelForm):
+    class Meta:
+        model = BuyOrder
+        fields = ('address','city','zip_code','contact',)
+        labels = {
+                    'address': 'Address',
+                    'city': 'City',
+                    'zip_code': 'Zip Code',
+                    'contact': 'Mobile Number',
         }
--- Rentalgo/products/models.py
+++ Rentalgo/products/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     prod_name = models.CharField(max_length=128, blank=False)
     description = models.TextField(blank=False)
+    quantity = models.PositiveIntegerField(default=1, blank=False)
     dprice = models.PositiveIntegerField()
     wprice = models.PositiveIntegerField()
     mprice = models.PositiveIntegerField()
@@ -22,3 +23,26 @@ class Product(models.Model):
 
     def __str__(self):
         return self.prod_name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='iorder')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='myorder')
+    address = models.CharField(max_length=512, blank=False)
+    city = models.CharField(max_length=64, blank=False)
+    zip_code = models.PositiveIntegerField()
+    contact = models.CharField(max_length=10, blank=False)
+    order_date = models.DateField()
+    date_range = models.CharField(max_length=128,blank=False)
+    cost = models.PositiveIntegerField(null=True)
+
+class BuyOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='buyorder')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='buyiorder')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='buymyorder')
+    address = models.CharField(max_length=512, blank=False)
+    city = models.CharField(max_length=64, blank=False)
+    zip_code = models.PositiveIntegerField()
+    contact = models.CharField(max_length=10, blank=False)
+    order_date = models.DateField()
+    cost = models.PositiveIntegerField(null=True)
